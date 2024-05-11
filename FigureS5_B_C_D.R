006f8a8f9f44fb07452b49d390e1d3c4127b0ccc
VannieXu <50305431+VannieXu@users.noreply.github.com>
2024-05-11 17:04:51 -0400

    Update and rename FigureS5_B_C.R to FigureS5_B_C_D.R

--- FigureS5_B_C_D.R
+++ FigureS5_B_C_D.R
@@ -46,3 +46,19 @@ dev.off()
 ```
 
 ```{r Pannel D}
+CTfeq<-tibble::as_tibble(pData(cds)) %>%
+  group_by(age,condition,celltype) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n))
+
+pdf(paste0("plots/",fname,"_PanelD_cell_proportion.pdf"))
+ ggplot(data = CTfeq,aes(x = freq, y = condition, fill = celltype))+
+          geom_bar(position="fill",stat="identity") + 
+          scale_y_discrete(limits=rev,labels = c('dKO','Control'))+
+          facet_wrap(~age,labeller = as_labeller(c(`12.5` = "E12.5",`14.5` = "E14.5")))+
+          labs(x = 'cell proportion',y = '')+ 
+          theme(panel.grid.major = element_blank(),strip.background = element_blank(), 
+                strip.text.x = element_text(size = 18),
+                panel.grid.minor = element_blank(),panel.background = element_blank())
+dev.off()
+```
